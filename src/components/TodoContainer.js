924af0727d01efaa468a681dcdd2cffd7e2d6b6a
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-12-06 23:49:55 +0100

    Add css

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import TodosList from './TodosList';
 import Header from './Header';
 import InputTodo from './InputTodo';
+import "../App.css";
 
 class TodoContainer extends React.Component {
   state = {
@@ -49,7 +50,7 @@ class TodoContainer extends React.Component {
 
   addTodoItem = (title) => {
     const newTodo = {
-      id: 4,
+      id: uuidv4(),
       title,
       completed: false,
     };
@@ -60,7 +61,8 @@ class TodoContainer extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="container">
+        <div className="inner">
         <Header />
         <InputTodo addTodoProps={this.addTodoItem} />
         <TodosList
@@ -68,6 +70,7 @@ class TodoContainer extends React.Component {
           handleChangeProps={this.handleChange}
           deleteTodoProps={this.delTodo}
         />
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoContainer from './components/TodoContainer';
 
+
 ReactDOM.render(
   <React.StrictMode>
     <TodoContainer />
