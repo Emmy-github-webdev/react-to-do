df80ff87235e8aff164ed016c1761a604948280c
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-07-20 00:37:36 +0100

    Add form validation

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -15,10 +15,14 @@ class InputTodo extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    this.props.addTodoProps(this.state.title);
-    this.setState({
-      title: ""
-    });
+    if(this.state.title.trim()){
+      this.props.addTodoProps(this.state.title);
+      this.setState({
+        title: ""
+      });
+    } else {
+      alert("please write item")
+    }
   };
 
   render(){
