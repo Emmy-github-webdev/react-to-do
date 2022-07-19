fd395881ab78da14c97200b033f09012255a1178
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-07-20 00:21:25 +0100

    Add new todo

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -3,6 +3,7 @@
 import React from "react";
 import TodosList from "./TodosList";
 import Head from "./Header";
+import InputTodo from "./InputTodo";
 
 class TodoContainer extends React.Component{
   state = {
@@ -50,10 +51,22 @@ class TodoContainer extends React.Component{
     })
   };
 
+  addTodoItem = (title) => {
+    const newTodo = {
+      id: 4,
+      title: title,
+      completed: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  };
+
   render(){
     return (
       <div>
         <Head />
+        <InputTodo addTodoProps={this.addTodoItem} />
         <TodosList 
         todos={this.state.todos} 
         handleChangeProps={this.handleChange} 
