5be7fcb453ddb7bfcee7b50f2c03abcaa0d5c3fc
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-07-23 02:06:53 +0100

    Implement SVG using react-icons

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 
 import React, { useState } from "react"
+import { FaPlusCircle } from "react-icons/fa"
 
 const InputTodo = props => {
   const [inputText, setInputText] = useState({
@@ -36,7 +37,16 @@ const InputTodo = props => {
         name="title"
         onChange={onChange}
       />
-      <button className="input-submit">Submit</button>
+      <button 
+        className="input-submit">
+        <FaPlusCircle 
+        style={{ 
+          color: "darkcyan", 
+          fontSize: "20px", 
+          marginTop: "2px" 
+        }}
+        />
+      </button>
     </form>
   )
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect  } from "react"
 import styles from "./TodoItem.module.css"
+import { FaTrash } from "react-icons/fa"
 
 const TodoItem = (props) => {
   const [editing, setEditing] = useState(false);
@@ -50,7 +51,12 @@ const TodoItem = (props) => {
           onChange={() => props.handleChangeProps(id)}
           /> 
           <button onClick={() => props.deleteTodoProps(id)}>
-            Delete
+            <FaTrash 
+            style={{ 
+              color: "orangered", 
+              fontSize: "16px" 
+            }}
+            />
           </button>
           <span style={completed ? completedStyle : null }>
             {title}
