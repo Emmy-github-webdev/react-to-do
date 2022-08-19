fe65f736f58b2411d16e72974b4c00490a00280f
emmanuel <ogahemmanuel75@gmail.com>
2022-08-19 22:05:06 +0100

    Update about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,8 +3,13 @@ import React from 'react';
 const About = () => (
   <div className="container">
     <div className="about__content">
-        <h2>Design by Emmanuel Ogah</h2>
-        <p>Software/DevOps Engineer, Ruby on Rails, NodeJS, Express, ReactJS, Redux, RestFul API, Containerization, Ansible, Terraform, Jenkins, AWS, Azure</p>
+      <h2>Design by Emmanuel Ogah</h2>
+      <p>
+        Software/DevOps Engineer
+      </p>
+      <p>
+        Ruby on Rails, NodeJS, ReactJS, Containerization, Ansible, Terraform, Jenkins, AWS, Azure
+      </p>
     </div>
   </div>
 );
